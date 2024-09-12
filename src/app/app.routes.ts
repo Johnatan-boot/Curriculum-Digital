ea8950aa4927d1be5169b0bd889f5d030e4dddf7
johnatan quenes dos santos <johnatan.quenes@gmail.com>
2024-09-12 20:07:12 -0300

    alterando configurações de frontend projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { HabilidadesComponent } from './habilidades/habilidades.component';
 import { ExperienciaComponent } from './experiencia/experiencia.component';
 import { ContatosComponent } from './contatos/contatos.component';
 import { EducacaoComponent } from './educacao/educacao.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 export const routes: Routes = [
   {path:'documentation', component: DocumentationComponent},
@@ -15,5 +16,6 @@ export const routes: Routes = [
   {path:'experiência', component: ExperienciaComponent},
   {path:'education', component: EducacaoComponent},
   {path:'contatos', component: ContatosComponent},
+  {path:'portfolio', component: PortfolioComponent},
 
 ];
